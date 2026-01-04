b77ce6f5fd418c4cdb8d155255c5539c3da6bce2
Atikou <3192664380@qq.com>
2026-01-04 17:35:27 +0800

    fix:域名

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "astro/config";
 const isProd = process.env.NODE_ENV === "production";
 
 export default defineConfig({
-  site: "https://atikou.github.io",
+  site: "https://polyg.dev/",
   base: isProd ? "/Projects/" : "/",
   markdown: {
     syntaxHighlight: "shiki",
