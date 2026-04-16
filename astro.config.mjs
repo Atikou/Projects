9090258f2d831e7f7e5993badd421e16a97698a6
Atikou <3192664380@qq.com>
2026-04-16 11:48:57 +0800

    fix:修复部署后路径错误

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 import { defineConfig } from "astro/config";
 
-const isProd = process.env.NODE_ENV === "production";
-const baseFromEnv = process.env.PUBLIC_BASE_PATH;
-
 export default defineConfig({
   site: "https://polyg.dev/",
-  base: isProd ? (baseFromEnv || "/Projects/") : "/",
+  base: "/",
   markdown: {
     syntaxHighlight: "shiki",
     shikiConfig: {
